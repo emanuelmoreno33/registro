746d7205b5ceee25b32669a607cf9aa32b1aadbe
emanuelmoreno33 <emanuel.moreno16@tectijuana.edu.mx>
2019-03-25 22:02:41 -0700

    primera version

---
+++ registro/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace registro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grupo1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.grupo1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // grupo1
+            // 
+            this.grupo1.Controls.Add(this.radioButton2);
+            this.grupo1.Controls.Add(this.radioButton1);
+            this.grupo1.Location = new System.Drawing.Point(12, 12);
+            this.grupo1.Name = "grupo1";
+            this.grupo1.Size = new System.Drawing.Size(217, 124);
+            this.grupo1.TabIndex = 1;
+            this.grupo1.TabStop = false;
+            this.grupo1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "No control";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(120, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nuevo alumno";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 59);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(162, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "alumno ya registrado";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Apellido";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 297);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(238, 297);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(214, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(472, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 260);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(469, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Alumnos registrados del dia";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 65);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 502);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grupo1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grupo1.ResumeLayout(false);
+            this.grupo1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox grupo1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ registro/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace registro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //conexion con la base de datos
+        SqlConnection conex = new SqlConnection("server=EMANUELMR-OMEN\\SQLEXPRESS;database=registro;integrated security = true");
+
+        private void agregaralumno(int nocontrol, string nom, string appat, string apmat)
+        {
+            conex.Open();
+            string cadena = "insert into alumnos(nocontrol,nombre,apepat,apemat) values("+nocontrol+",'"+nom+"','"+appat+"','"+apmat+"')";
+            SqlCommand comando = new SqlCommand(cadena, conex);
+            comando.ExecuteNonQuery();
+            conex.Close();
+        }
+
+        private bool verificaralumno(int nocontrol)
+        {
+            bool bandera = false;
+            conex.Open();
+            string cadena = "select apepat,apemat from alumnos where nocontrol ="+nocontrol;
+            SqlCommand comando = new SqlCommand(cadena, conex);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                bandera = true;
+                textBox3.Text = registro["apepat"].ToString();
+                textBox4.Text = registro["apemat"].ToString();
+            }
+            else
+            {
+                bandera = false;
+            }
+            conex.Close();
+            return bandera;
+        }
+
+        private void registrardia(int nocontrol)
+        {
+            conex.Open();
+            string cadena = "insert into registrodia (nocontrol,fecha) values (" + nocontrol + ",GETDATE())";
+            SqlCommand comando = new SqlCommand(cadena, conex);
+            comando.ExecuteNonQuery();
+            conex.Close();
+        }
+
+        private void limpiar()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                //solo se agrega al dia
+                bool encontrado = verificaralumno(Convert.ToInt32(textBox1.Text));
+                if (encontrado == true)
+                {
+                    registrardia(Convert.ToInt32(textBox1.Text));
+                    listBox1.Items.Add(textBox1.Text + " " + textBox3.Text + " " + textBox4.Text);
+                    limpiar();
+                }
+                else
+                {
+                    MessageBox.Show("El alumno no fue encontrado");
+                }
+
+            }
+            else if (radioButton1.Checked == true)
+            {
+                //se registra y agrega al dia
+                bool encontrado = verificaralumno(Convert.ToInt32(textBox1.Text));
+                if (encontrado == false)
+                {
+                    agregaralumno(Convert.ToInt32(textBox1.Text),textBox2.Text,textBox3.Text,textBox4.Text);
+                    registrardia(Convert.ToInt32(textBox1.Text));
+                    listBox1.Items.Add(textBox1.Text + " " + textBox3.Text + " " + textBox4.Text);
+                    limpiar();
+                }
+                else
+                {
+                    MessageBox.Show("El alumno ya habia sido registrado");
+                }
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked == true)
+            {
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+            }
+        }
+    }
+}
